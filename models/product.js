007f38373efae12aa1e612f2914201ece6a5398d
Richard Tom <tomrichard14@gmail.com>
2018-09-25 09:31:36 -0700

    inital db schema created

---
+++ models/product.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  type: String,
+  productType: { type: String, enum: ['razor', 'blade', 'brush', 'lather', 'aftershave', 'additonalcare']},
+  brand: String,
+  model: String,
+});
+
+ProductSchema.set('toObject', {
+  virtuals: true, // include built-in virtual `id`
+  versionKey: false, // remove `__v` version key
+  transform: (doc, ret) => {
+    delete ret._id; // delete `_id`
+  }
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
---
+++ models/shave.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const ShaveSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  razorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+  bladeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+  brushId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+  latherId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+  aftershaveId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+  additionalCare: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+  rating: Number,
+  date: { type:Date, required:true }
+});
+
+ShaveSchema.set('toObject', {
+  virtuals: true, // include built-in virtual `id`
+  versionKey: false, // remove `__v` version key
+  transform: (doc, ret) => {
+    delete ret._id; // delete `_id`
+  }
+});
+
+module.exports = mongoose.model('Shave', ShaveSchema);
