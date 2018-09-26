aa4551218bd4964281193c64b57e444a7ea7ed57
dghero <devingreyhero@gmail.com>
2018-09-26 13:21:14 -0700

    Begin GET /shaves

--- routes/shaves.js
+++ routes/shaves.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 const Shave = require('../models/shave');
+const UserProduct = require('../models/userProduct');
 const User = require('../models/user');
 
 const jwtAuth = passport.authenticate('jwt', { session: false });
@@ -15,21 +16,47 @@ router.get('/', jwtAuth, (req, res, next) => {
     err.status = 400;
     return next(err);
   }
+
+  let userProducts = {};
+  const productTypes = ['razor', 'blade', 'brush','lather','aftershave','additionalCare'];
+  UserProduct.findOne({userId})
+    .then(results =>{
+      console.log(results);
+      productTypes.forEach(prodType =>{
+        userProducts[prodType] = results[prodType];
+      });
+      // console.log(shaveHistory);
+      return Shave.find({userId});
+    })
+    // .populate('razorId.productId')
+    .then(results =>{
+      productTypes.forEach(prodType =>{
+        // console.log('userProd ', userProducts[prodType]);
+        const resIdx = `${prodType}Id`;
+        // console.log('res0', results[0]);
+        // console.log(`res0 ${resIdx}`, results[0][resIdx]);
+        // const item = shaveHistory[prodType].filter(histItem => histItem.)
+        // results[prodType] = 
+      });
+      // console.log(results);
+      res.json(results);
+    })
+    .catch(err =>{
+      next(err);
+    });
+
 });
 
-// eslint-disable-next-line no-unused-vars
 router.post('/', jwtAuth, (req, res, next) => {
-
+  
 });
 
-// eslint-disable-next-line no-unused-vars
 router.put('/:id', jwtAuth, (req, res, next) => {
-
+  
 });
 
-// eslint-disable-next-line no-unused-vars
 router.delete('/:id', jwtAuth, (req, res, next) => {
-
+  
 });
 
 module.exports = router;
