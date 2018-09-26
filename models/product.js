de88a98953cfa10bd4b08672298b4385a778e704
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-09-26 12:25:36 -0600

    update package-lock.json with linting requirements after merge from master

--- models/product.js
+++ models/product.js
@@ -11,7 +11,7 @@ ProductSchema.set('toObject', {
   virtuals: true, // include built-in virtual `id`
   versionKey: false, // remove `__v` version key
   transform: (doc, ret) => {
-    delete ret._id; // eslint-disable-line no-param-reassign, no-underscore-dangle
+    delete ret._id; // eslint-disable-line no-param-reassign
   },
 });
 
