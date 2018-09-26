6b96aac2ba26a5af82592baffe95ce0622af2159
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-09-26 16:18:50 -0600

    update validation method for empty string fields and add comments

--- routes/userProducts.js
+++ routes/userProducts.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const Product = require('../models/product');
 const UserProduct = require('../models/userProduct');
-const User = require('../models/user');
 
 const jwtAuth = passport.authenticate('jwt', { session: false });
 
@@ -26,68 +25,85 @@ router.post('/', jwtAuth, (req, res, next) => {
     return next(err);
   }
 
-  const {brand, model, productType, comment, nickname, subtype} = req.body;
-  let ref;
-
+  // validate all required fields were submitted
   const requiredFields = ['brand', 'model', 'productType'];
   const missingField = requiredFields.find(field => !(field in req.body));
-
   if (missingField) {
     const err = new Error(`Missing '${missingField}' in request body`);
     err.status = 422;
+    err.reason = 'ValidationError';
+    err.location = `${missingField}`;
     return next(err);
   }
 
-  if (!brand) {
-    const err = new Error('Missing \'brand\' in request');
+  // validate fields that should be strings are actually of type 'string'
+  const stringFields = ['brand', 'model', 'productType', 'subtype', 'comment', 'nickname'];
+  const nonStringField = stringFields.find(field => ((req.body[field]) && (typeof req.body[field] !== 'string')));
+  if (nonStringField) {
+    const err = new Error(`The \`${nonStringField}\` must be of type \`string\``);
     err.status = 422;
-    return next(err);
-  }
-  if (!model) {
-    const err = new Error('Missing \'model\' in request');
-    err.status = 422;
-    return next(err);
-  }
-  if (!productType) {
-    const err = new Error('Missing \'productType\' in request');
-    err.status = 422;
+    err.reason = 'ValidationError';
+    err.location = `${nonStringField}`;
     return next(err);
   }
 
-  Product.findOne({brand, model, productType, subtype})
-    .then(result => {
+  // validate required fields are not empty strings
+  const emptyField = requiredFields.find(field => req.body[field].trim().length === 0);
+  if (emptyField) {
+    const err = new Error(`The \`${emptyField}\` cannot be an empty string`);
+    err.status = 422;
+    err.reason = 'ValidationError';
+    err.location = `${emptyField}`;
+    return next(err);
+  }
+
+  // required fields have been validated as existing
+  // non-required fields will be undefined and will
+  // not pass through into the database
+  const {
+    brand, model, productType, comment, nickname, subtype,
+  } = req.body;
+  // will hold the global Product reference once found or created
+  let ref;
+
+  // Check if new product already exists in globalProducts
+  Product.findOne({
+    brand, model, productType, subtype,
+  })
+    .then((result) => {
+      // if globalProduct already exists, use reference
       if (result) {
         return result;
-      } else {
-        return Product.create({brand, model, productType, subtype});
       }
-      return Product.create({ brand, model, productType });
+      // if not, add to globalProduct
+      return Product.create({
+        brand, model, productType, subtype,
+      });
     })
     .then((result) => {
+      // refers to a pre-existing or newly-created globalProduct
       ref = result;
       return UserProduct.findOne({ userId });
     })
     .then((userProduct) => {
-      const doesExistArray = userProduct[`${productType}`].filter(item => JSON.stringify(item.productId) === JSON.stringify(ref._id));
+      const doesExistArray = userProduct[productType].filter(item => (
+        JSON.stringify(item.productId) === JSON.stringify(ref._id)
+      ));
 
       if (doesExistArray.length > 0) {
-        const err = new Error('Item already exist');
+        const err = new Error('Item already exists');
         err.status = 400;
         return Promise.reject(err);
       }
-      userProduct[`${productType}`].push({ productId: ref._id, comment, nickname });
+      userProduct[productType].push({ productId: ref._id, comment, nickname });
       return userProduct.save();
     })
-    .then(() => UserProduct.findOne({userId})
+    .then(() => UserProduct.findOne({ userId })
       .populate('razor.productId blade.productId brush.productId lather.productId aftershave.productId additionalcare.productId'))
     .then((result) => {
       res.status(201).json(result);
     })
     .catch(err => next(err));
-
-  // Check if new product already exists in globalProducts
-  // if so, get reference to globalproduct, refer to it when adding userProduct
-  // if not, add to globalProduct, refer to it when adding userProduct
 });
 
 // eslint-disable-next-line no-unused-vars
