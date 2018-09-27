2c83e9049e6c6a5e19589edbae511a083603c746
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-09-26 20:51:08 -0600

    update naming and add comments for shave history GET endpoint

--- routes/shaves.js
+++ routes/shaves.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const Shave = require('../models/shave');
 const UserProduct = require('../models/userProduct');
-const User = require('../models/user');
 
 const jwtAuth = passport.authenticate('jwt', { session: false });
 
@@ -17,64 +16,75 @@ router.get('/', jwtAuth, (req, res, next) => {
     return next(err);
   }
 
-  let userProds = {};
-  const productTypes = ['razor', 'blade', 'brush','lather','aftershave','additionalCare'];
+  const userProds = {};
+  const productTypes = ['razor', 'blade', 'brush', 'lather', 'aftershave', 'additionalCare'];
   const userProductProperties = ['nickname', 'comment'];
   const globalProductProperties = ['subtype', 'productType', 'brand', 'model', 'id'];
-  let returnObj = [];
-  UserProduct.findOne({userId})
+  const shaveHistory = [];
+
+  UserProduct.findOne({ userId })
     .populate('razor.productId blade.productId brush.productId lather.productId aftershave.productId additionalCare.productId')
-    .then(results =>{
-      productTypes.forEach(prodType =>{
+    .then((results) => {
+      productTypes.forEach((prodType) => {
+        // initialize arrays to hold each productType for the user
         userProds[prodType] = [];
-        
-        for(let i = 0; i < results[prodType].length; i++){
+
+        // loop through each productType array returned in the user's UserProduct db result
+        for (let i = 0; i < results[prodType].length; i += 1) {
+          // initialize a new object to hold each entry
           userProds[prodType][i] = {};
 
-          userProductProperties.forEach(property =>{
+          // copy the user's custom properties into the new object
+          userProductProperties.forEach((property) => {
             userProds[prodType][i][property] = results[prodType][i][property];
           });
-          globalProductProperties.forEach(property =>{
+          // copy the global properties up to the same nesting level as the custom properties
+          globalProductProperties.forEach((property) => {
             userProds[prodType][i][property] = results[prodType][i].productId[property];
-          })
-          //manual handling of IDs
+          });
+          // manual handling of IDs
           userProds[prodType][i].productId = userProds[prodType][i].id;
           userProds[prodType][i].id = results[prodType][i].id;
         }
       });
-      // console.log(userProds);
-      return Shave.find({userId});
+      // get all shave events for the user
+      return Shave.find({ userId });
     })
-    .then(results =>{
-      for(let i = 0; i < results.length; i++){
-        returnObj[i] = {}
-        productTypes.forEach(prodType =>{
-          const resIdx = `${prodType}Id`;
-          const shaveItemId = results[i][resIdx];
+    .then((shaveEvents) => {
+      for (let i = 0; i < shaveEvents.length; i += 1) {
+        // populate the ids returned in shaveEvents with the
+        // constructed data from userProds and save the
+        // hydrated data to shaveHistory
+        shaveHistory[i] = {};
+        productTypes.forEach((prodType) => {
+          const productKey = `${prodType}Id`;
+          const shaveItemId = shaveEvents[i][productKey];
           const item = userProds[prodType]
             .filter(prod => JSON.stringify(prod.id) === JSON.stringify(shaveItemId))[0];
-            
-          returnObj[i][resIdx] = item ? item : null;
+
+          shaveHistory[i][productKey] = item || null;
         });
       }
-      res.json(returnObj);
+      res.json(shaveHistory);
     })
-    .catch(err =>{
+    .catch((err) => {
       next(err);
     });
-
 });
 
+// eslint-disable-next-line no-unused-vars
 router.post('/', jwtAuth, (req, res, next) => {
-  
+
 });
 
+// eslint-disable-next-line no-unused-vars
 router.put('/:id', jwtAuth, (req, res, next) => {
-  
+
 });
 
+// eslint-disable-next-line no-unused-vars
 router.delete('/:id', jwtAuth, (req, res, next) => {
-  
+
 });
 
 module.exports = router;
