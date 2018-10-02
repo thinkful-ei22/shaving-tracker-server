ae7496216138ba395eaef9f663dfa82ffabee5aa
Richard Tom <tomrichard14@gmail.com>
2018-10-02 09:39:29 -0700

    removed some console.logs and returning a single image now

--- routes/images.js
+++ routes/images.js
@@ -9,12 +9,11 @@ const formDataParser = formData.parse();
 router.post('/upload', formDataParser, (req, res, next) => {
   const values = Object.values(req.files);
   const promises = values.map(image => cloudinary.uploader.upload(image.path));
-  console.log(promises);
   Promise
     .all(promises)
     .then((result) => {
-      console.log(result);
-      res.json(result);
+      console.log(result[0]);
+      res.json(result[0]);
     })
     .catch(err => next(err));
 });
