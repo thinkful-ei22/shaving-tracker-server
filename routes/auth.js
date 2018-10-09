479d0e75a19e1bea628aebbf7cd27f9b6f5209bb
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-10-09 10:33:56 -0600

    move createAuthToken function into its own module to make it available for tests

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,12 @@
 const passport = require('passport');
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const { JWT_SECRET, JWT_EXPIRY } = require('../config');
+const { createAuthToken } = require('../utils/auth');
 
 const router = express.Router();
 
 const options = { session: false, failWithError: true };
 
 const localAuth = passport.authenticate('local', options);
-
-function createAuthToken(user) {
-  return jwt.sign({ user }, JWT_SECRET, {
-    subject: user.username,
-    expiresIn: JWT_EXPIRY,
-    algorithm: 'HS256',
-  });
-}
 
 router.post('/login', localAuth, (req, res) => {
   const authToken = createAuthToken(req.user);
